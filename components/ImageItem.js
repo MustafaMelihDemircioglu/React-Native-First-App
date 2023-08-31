47073ae85c73ad046de35171152de0e871ec9b19
Mustafa Melih Demircioğlu <mustafamelih9@gmail.com>
2023-08-31 17:23:48 +0300

    Commit

--- components/ImageItem.js
+++ components/ImageItem.js
@@ -1,40 +1,46 @@
 import React, { useState } from "react";
 import { View, Image, ActivityIndicator } from 'react-native';
+import { useEffect } from "react";
 
 function ImageItem(props) {
     const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        console.log("Yes")
+    }, [loading])
 
     function onLoading(value) {
         setLoading(value);
     }
 
     /* return (
-        <View style={{ width: '100%', aspectRatio: props.aspectRatio}}>
-            {loading ? (
-                <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height:'100%'}}>
-                    <ActivityIndicator color="blue"></ActivityIndicator>
+        <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
+            {loading ?
+                <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height: '100%' }}>
+                    <ActivityIndicator size='large'></ActivityIndicator>
                 </View>
-            ) : (
-                <Image style={{ height: '100%', width:'100%', borderRadius: 8, }}
+                :
+                <Image style={{ height: '100%', width: '100%', borderRadius: 8, }}
                     source={{ uri: props.sourceURL }}
                     onLoadStart={() => onLoading(true)}
-                    onLoadEnd={() => onLoading(false)}>
-                </Image>
-            )}
-    
+                    onLoadEnd={() => onLoading(false)}
+                />
+
+            }
+
         </View>
     ); */
 
     return (
-        <View style={{ width: '100%', aspectRatio: props.aspectRatio}}>
+        <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
             {loading &&
-                <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height:'100%'}}>
+                <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height: '100%' }}>
                     <ActivityIndicator color="blue"></ActivityIndicator>
                 </View>
             }
 
             {
-                <Image style={{ height: '100%', width:'100%', borderRadius: 8, }}
+                <Image style={{ height: '100%', width: '100%', borderRadius: 8, }}
                     source={{ uri: props.sourceURL }}
                     onLoadStart={() => onLoading(true)}
                     onLoadEnd={() => onLoading(false)}>
