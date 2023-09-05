f5076ec1d8dc792544e390366397e025b8c9efa6
Mustafa Melih Demircioğlu <mustafamelih9@gmail.com>
2023-09-05 10:48:32 +0300

    Added image to show when unable to fetch data from api.

--- Screens/CatDetail.js
+++ Screens/CatDetail.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useState } from "react";
-import { Text, View, TextInput, FlatList, SafeAreaView, ActivityIndicator, Dimensions, ScrollView, Image } from "react-native";
+import { Text, View, TextInput, FlatList, SafeAreaView, ActivityIndicator, Dimensions, ScrollView, Image, StyleSheet } from "react-native";
 import { appStyles as styles } from "../styles";
 import { useEffect } from "react";
 import ImageItem from "../components/ImageItem";
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-reanimated-table';
 
 function CatDetail({ route, navigation }) {
 
     const details = route.params;
 
-    const aspectRatioValue = details.image.width / details.image.height
+    let aspectRatioValue = 3/2;
+    let imageURL;
+    let notAvaliable = true;
+
+    if ("image" in details) {
+        aspectRatioValue = details.image.width / details.image.height;
+        imageURL = details.image.url;
+        notAvaliable = false;
+    }
+
+    const tableHead = ['Characteristics', 'Rating'];
+
+    const tableTitle = ['Adaptability', 'Affection Level', 'Child Friendly', 'Energy Level', 'Grooming', 'Health Issues', 'Intelligence'];
+
+    const tableData = [[details.adaptability], [details.affection_level], [details.child_friendly], [details.energy_level], [details.grooming], [details.health_issues], [details.intelligence]]
 
     /* navigation.setOptions({
         title: details.name,
     }); */
 
-    /* useEffect(() => {
+    useLayoutEffect(() => {
         // Set the header title for Screen 3 immediately
         navigation.setOptions({
-          title: details.name,
+            title: details.name,
         });
-      }, []); */
+    }, []);
 
     return (
         <SafeAreaView style={styles.safeArea}>
@@ -33,18 +48,29 @@ function CatDetail({ route, navigation }) {
                     {details.name}
                 </Text>
 
-                <ImageItem aspectRatio={aspectRatioValue} sourceURL={details.image.url}></ImageItem>
+                <ImageItem aspectRatio={aspectRatioValue} sourceURL={imageURL} notAvaliable={notAvaliable}></ImageItem>
 
-                <View style={{ flexDirection: 'row' }}>
-                    <Text>hey</Text>
-                    <Text>ho</Text>
+                <View style={styles3.container}>
+                    <Table borderStyle={{ borderWidth: 1 }} >
+                        <Row data={tableHead} style={styles3.head} textStyle={styles3.text} flexArr={[1, 1]} />
+                        <TableWrapper style={styles3.wrapper}>
+                            <Col data={tableTitle} style={styles3.title} textStyle={styles3.text} />
+                            <Rows data={tableData} style={styles3.row} textStyle={styles3.text} flexArr={[1]} />
+                        </TableWrapper>
+                    </Table>
                 </View>
-
-                
-
             </ScrollView>
         </SafeAreaView >
     );
 }
 
 export default CatDetail;
+
+const styles3 = StyleSheet.create({
+    container: { flex: 1, paddingTop: 16 },
+    head: { height: 40, backgroundColor: '#f1f8ff' },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, backgroundColor: '#f6f8fa' },
+    row: { height: 28 },
+    text: { textAlign: 'center' }
+});
--- components/ImageItem.js
+++ components/ImageItem.js
@@ -6,32 +6,50 @@ function ImageItem(props) {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        console.log("Yes")
     }, [loading])
 
     function onLoading(value) {
         setLoading(value);
     }
 
-    /* return (
-        <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
-            {loading ?
-                <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height: '100%' }}>
-                    <ActivityIndicator size='large'></ActivityIndicator>
-                </View>
-                :
-                <Image style={{ height: '100%', width: '100%', borderRadius: 8, }}
-                    source={{ uri: props.sourceURL }}
-                    onLoadStart={() => onLoading(true)}
-                    onLoadEnd={() => onLoading(false)}
+    if (props.notAvaliable) {
+        return (
+            <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
+
+                <Image style={{
+                    height: '100%', width: '100%', borderRadius: 8,
+                }}
+                    source={require('../assets/no-image.png')}
                 />
 
-            }
-
-        </View>
-    ); */
+            </View>
+        );
+    }
 
     return (
+        <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
+
+            <View style={{
+                justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height: '100%',
+                display: loading ? "flex" : 'none'
+            }}>
+                <ActivityIndicator size='large'></ActivityIndicator>
+            </View>
+
+            <Image style={{
+                height: '100%', width: '100%', borderRadius: 8,
+                display: loading ? "none" : 'flex'
+
+            }}
+                source={{ uri: props.sourceURL }}
+                onLoadStart={() => onLoading(true)}
+                onLoadEnd={() => onLoading(false)}
+            />
+
+        </View>
+    );
+
+    /* return (
         <View style={{ width: '100%', aspectRatio: props.aspectRatio }}>
             {loading &&
                 <View style={{ justifyContent: 'center', alignSelf: 'center', alignContent: 'center', width: '100%', height: '100%' }}>
@@ -48,7 +66,7 @@ function ImageItem(props) {
             }
 
         </View>
-    );
+    ); */
 
 }
 
